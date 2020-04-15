22aa0359af6fa5da82cee99e067e852caa7a557b
Meeluck <50030554+Meeluck@users.noreply.github.com>
2020-04-15 15:32:49 +0300

    Add project files.

---
+++ ElectronicJournal_DataBase/Model/User.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ElectronicJournal_DataBase.Model
+{
+	class User
+	{
+	}
+}
---
+++ ElectronicJournal_DataBase/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ElectronicJournal_DataBase
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
